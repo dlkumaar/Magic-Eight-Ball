47121cf6b527230d34eefe3b7bd3fa18282f15f8
Rahul Kumar <dev.rkumaar@gmail.com>
2021-01-30 13:26:30 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+"use strict";
+
+let userName = "Rahul";
+userName ? console.log(`Hello ${userName}!`) : console.log("Hello!");
+
+const userQuestion = "What's the lucky number today?";
+console.log(userQuestion);
+
+const randomNumber = Math.floor(Math.random() * 8);
+console.log(randomNumber);
+
+let eightBall = "";
+
+switch (randomNumber) {
+  case 1:
+    eightBall = "It is certain";
+    break;
+
+  case 2:
+    eightBall = "It is decidedly so";
+    break;
+
+  case 3:
+    eightBall = "Reply hazy try again";
+    break;
+
+  case 4:
+    eightBall = "Cannot predict now";
+    break;
+
+  case 5:
+    eightBall = "Do not count on it";
+    break;
+
+  case 6:
+    eightBall = "My sources say no";
+    break;
+
+  case 7:
+    eightBall = "Outlook not so good";
+    break;
+
+  case 8:
+    eightBall = "Signs point to yes";
+    break;
+}
+
+console.log(eightBall);
